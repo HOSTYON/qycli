f617ba5ee2bbe597a9d14909572c320435bbd31b
Micha Cassola <mich@casso.la>
2025-04-08 14:24:27 +0200

    Changed site type handling

---
+++ cnf/wp/wp-config-redirect.php
@@ -0,0 +1,8 @@
+<?php
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Location of your WordPress configuration. */
+require_once(ABSPATH . '../conf/wp-config.php');
